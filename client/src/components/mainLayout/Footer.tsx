3b8e5b931b65dafb9a2f90b08d8f235ff96de69d
SP.Win <supawit.promma6256@gmail.com>
2024-10-09 22:23:44 +0700

    Change footer

--- client/src/components/mainLayout/Footer.tsx
+++ client/src/components/mainLayout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-white py-2 px-4 lg:px-8 text-center text-sm text-gray-500">
-      The source code is available on <a href="https://github.com/WinZ5/umb888" className="text-blue-500">GitHub</a>.
+      Developed by Team: "Team and Name" | 953212 Database  Systems and Database System design | The source code is available on <a href="https://github.com/WinZ5/umb888" className="text-blue-500">GitHub</a>.
     </footer>
   )
 }
